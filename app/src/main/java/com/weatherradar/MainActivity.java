d8af75f51c76b67ebcb60bdc5ef0e37584abfdbd
VisualGhost <weatherrocket@gmail.com>
2017-04-25 20:29:10 +0300

    Google Map is added

--- app/src/main/java/com/weatherradar/MainActivity.java
+++ app/src/main/java/com/weatherradar/MainActivity.java
@@ -1,7 +1,9 @@
 package com.weatherradar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.map_container);
+        if (fragment == null) {
+
+            FragmentTransaction transition = getFragmentManager().beginTransaction();
+            transition.replace(R.id.map_container, new WeatherMap());
+            transition.commit();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions,
+                                           int[] grantResults) {
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.map_container);
+        WeatherMap weatherMap = (WeatherMap) fragment;
+        weatherMap.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
